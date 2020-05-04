6b81f4060cfbe2cf18a9856f878e836642fec0c5
Jim Greer <jimgreer@gmail.com>
2020-05-03 20:07:57 -0700

    Yet another try at logging

--- index.js
+++ index.js
@@ -17,28 +17,28 @@ const message = `\nCloses #${issueNumber}`;
 body = body.concat(message);
 
 console.log(process.env);
-const graphqlWithAuth = graphql.defaults({
-  headers: {
-    authorization: `token ${process.env.GITHUB_TOKEN}`
-  }
-});
+// const graphqlWithAuth = graphql.defaults({
+//   headers: {
+//     authorization: `token ${process.env.GITHUB_TOKEN}`
+//   }
+// });
 
-async function mutatePullRequest() {
-  const result = await graphqlWithAuth(
-  `
-  mutation updatePullRequestBody {
-    updatePullRequest(input: {
-      pullRequestId:"${pullRequestId}",
-      body:"${body}"
-    }) {
-      pullRequest {
-        id
-      }
-    }
-  }
-  `
-  );
-}
+// async function mutatePullRequest() {
+//   const result = await graphqlWithAuth(
+//   `
+//   mutation updatePullRequestBody {
+//     updatePullRequest(input: {
+//       pullRequestId:"${pullRequestId}",
+//       body:"${body}"
+//     }) {
+//       pullRequest {
+//         id
+//       }
+//     }
+//   }
+//   `
+//   );
+// }
 
 // mutatePullRequest();
 
