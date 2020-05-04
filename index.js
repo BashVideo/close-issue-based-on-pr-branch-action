4965e269e7ca998118004b3b522ee60b6844aa1a
Jim Greer <jimgreer@gmail.com>
2020-05-03 19:59:57 -0700

    GraphQL to update PR body

--- index.js
+++ index.js
@@ -1,19 +1,43 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-
+const { graphql } = require("@octokit/graphql")
 const fs = require('fs');
+
+// parse Github's description of triggering event
 const ev = JSON.parse(
   fs.readFileSync(process.env.GITHUB_EVENT_PATH, 'utf8')
 );
 
-// console.log(ev);
-const prNum = ev.pull_request.number;
-console.log(`pull request: ${prNum}`);
+const owner = ev.repository.owner.login;
+const repo = ev.repository.name;
+const pullRequestId = ev.pull_request.id;
+let body = ev.pull_request.body;
 
 const branchName = ev.pull_request.head.ref;
+const issueNumber = branchName.match(/^[0-9]+/)[0];
+const message = `\nCloses #${issueNumber}`;
+body = body.concat(message);
 
-branchName.match(/^[0-9]+/)[0];
-num = branchName.match(/^[0-9]+/)[0];
-message = `Closes #${num}`;
-console.log(`message: ${message}`);
+const graphqlWithAuth = graphql.defaults({
+  headers: {
+    authorization: `token ${process.env.GITHUB_TOKEN}`
+  }
+});
+
+async function mutatePullRequest() {
+  const result = await graphqlWithAuth(
+  `
+  mutation updatePullRequestBody {
+    updatePullRequest(input: {
+      pullRequestId:"${pullRequestId}",
+      body:"${body}"
+    }) {
+      pullRequest {
+        id
+      }
+    }
+  }
+  `
+  );
+}
+
+mutatePullRequest();
 
