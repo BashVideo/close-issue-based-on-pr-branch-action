444c6271b98ef5a24547412aebaeabd5ec180667
Jim Greer <jimgreer@gmail.com>
2020-05-03 18:27:36 -0700

    syntax

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-const fs = require('fs')
+const fs = require('fs');
 const ev = JSON.parse(
   fs.readFileSync(process.env.GITHUB_EVENT_PATH, 'utf8')
-)
+);
 
-console.log(ev)
-const prNum = ev.pull_request.number
-console.log(`pull request ${prNum}`)
+// console.log(ev);
+const prNum = ev.pull_request.number;
+console.log(`pull request: ${prNum}`);
 
-const branchName = ev.pull_request.head.ref
-console.log(`branch name ${branchName}`) 
+const branchName = ev.pull_request.head.ref;
+console.log(`branch name: ${branchName}`);
 
-branchName.match(/^[0-9]+/)[0]
-num = branchName.match(/^[0-9]+/)[0]
-message = `Closes #${num}`
+branchName.match(/^[0-9]+/)[0];
+num = branchName.match(/^[0-9]+/)[0];
+message = `Closes #${num}`;
+console.log(`message ${message}`)
