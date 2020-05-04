9f265f363af6157d7167df16c722be840f93fc6e
Jim Greer <jimgreer@gmail.com>
2020-05-03 18:18:34 -0700

    log github event json

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const fs = require('fs')
 const ev = JSON.parse(
   fs.readFileSync(process.env.GITHUB_EVENT_PATH, 'utf8')
 )
+
+console.log(ev)
 const prNum = ev.pull_request.number
 console.log(`pull request ${prNum}`)
 
-const branchName = ev.branchName
+const branchName = ev.
 console.log(`branch name ${branchName}`) 
 
-branchName.match(/^[0-9]+/)[0]
-num = branchName.match(/^[0-9]+/)[0]
-message = `Closes #${num}`
+// branchName.match(/^[0-9]+/)[0]
+// num = branchName.match(/^[0-9]+/)[0]
+// message = `Closes #${num}`
